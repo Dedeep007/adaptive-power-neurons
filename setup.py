253c2849e4a6e23ecab7ca4e0d50a69d563fda04
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-07 23:33:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="adaptive-power-neurons", 
+    name="adaptive_power_neurons", 
     version="0.1.0",  
     packages=find_packages(), 
     description="A machine learning model with adaptive power neurons for polynomial feature processing.",
