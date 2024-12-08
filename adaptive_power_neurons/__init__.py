29b1728bb14296a537b35ddd04da37293a08c4fe
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-08 14:16:31 +0530

    Update __init__.py

--- adaptive_power_neurons/__init__.py
+++ adaptive_power_neurons/__init__.py
@@ -2,6 +2,6 @@
 
 from .activation import Activation
 from .optimizer import SGD, Optimizer
-from .adaptive_power_neuron import AdaptivePowerNeuron
+from .adaptive_power_neurons import AdaptivePowerNeuron
 from .dense_layer import DenseLayer
 from .adaptive_power_model import AdaptivePowerModel
