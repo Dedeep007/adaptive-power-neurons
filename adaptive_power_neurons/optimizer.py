e2e54810ebd477f2c65c637c0627d1f2a25eaaab
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-08 13:35:51 +0530

    Update optimizer.py

--- adaptive_power_neurons/optimizer.py
+++ adaptive_power_neurons/optimizer.py
@@ -1,13 +1,23 @@
+# optimizer.py
+
 class Optimizer:
     """
-    An optimizer class that allows the adjustment of learning rate, power, and indexing power during training.
+    Base class for optimizers.
     """
-    def __init__(self, learning_rate=0.001, max_power=3, indexing_rate=0.1):
-        self.learning_rate = learning_rate
-        self.max_power = max_power
-        self.indexing_rate = indexing_rate
+    def update(self, params, grads):
+        """
+        Updates parameters based on gradients.
+        """
+        raise NotImplementedError("This method should be implemented by subclasses.")
 
-    def apply_optimizer(self, perceptron, layer_avg_params):
-        perceptron.learning_rate = (self.learning_rate + layer_avg_params['learning_rate']) / 2
-        perceptron.max_power = (self.max_power + layer_avg_params['max_power']) / 2
-        perceptron.indexing_rate = (self.indexing_rate + layer_avg_params['indexing_rate']) / 2
+
+class SGD(Optimizer):
+    """
+    Stochastic Gradient Descent optimizer.
+    """
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def update(self, params, grads):
+        for param, grad in zip(params, grads):
+            param -= self.learning_rate * grad
