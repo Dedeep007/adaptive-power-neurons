00392e59368a57f71e160bf74e772c3a051b446e
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-08 12:02:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="adaptive-power-neurons", 
-    version="0.1.6", 
+    name="adaptive-power-neurons",
+    version="0.1.0.0",
+    description="A neural network library using Adaptive Power Neurons.",
+    author="Your Name",
     packages=find_packages(),
-    description="A machine learning model with adaptive power neurons for polynomial feature processing.",
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Dedeep007/adaptive-power-neurons",
-    author="Dedeep Vasireddy", 
-    author_email="vasireddydedeep@gmail.com",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "numpy>=1.18.5",
-    ],
-    python_requires='>=3.6',
+    install_requires=["numpy"],
+    python_requires=">=3.6",
 )
