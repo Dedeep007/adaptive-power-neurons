edf549e80e11645b62526b298af2fc189594316d
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-08 14:46:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     # Basic metadata
     name=('adaptive_power_neurons')  # Name of the package
-    version='0.1.7.7.8',  # Version of the package (ensure this is updated on each release)
+    version='0.1.7.7.9',  # Version of the package (ensure this is updated on each release)
     author='Dedeep Vasireddy',  # Your name or organization
     author_email='vasireddydedeep@gmail.com',  # Your email
     description='A neural network model using adaptive power neurons for regression and classification',
