9b680bc2e2ec445ce4d8d9e17674f07374ef7298
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-08 15:36:38 +0530

    Update dense_layer.py

--- adaptive_power_neurons/dense_layer.py
+++ adaptive_power_neurons/dense_layer.py
@@ -4,6 +4,7 @@ from .activation import Activation
 
 class DenseLayer:
     def __init__(self, input_dim, output_dim, max_power, optimizer, indexing_rate, activation="identity"):
+        from .adaptive_power_neurons import AdaptivePowerNeuron
         self.neurons = [
             AdaptivePowerNeuron(input_dim, max_power, optimizer, indexing_rate)
             for _ in range(output_dim)
