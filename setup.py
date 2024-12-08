8b8fe2ee3ae7fd5425debf5656a46a464940f1a3
Dedeep007 <vasireddydedeep@gmail.com>
2024-12-08 12:03:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="adaptive-power-neurons",
-    version="0.1.0.0",
+    version="0.1.0.0.0",
     description="A neural network library using Adaptive Power Neurons.",
     author="Your Name",
     packages=find_packages(),
